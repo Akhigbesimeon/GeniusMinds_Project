50a6ee094a500a008bd482453d420900a0bb6153
Akhigbesimeon <s.akhigbe@alustudent.com>
2025-03-11 18:56:58 +0200

    Made changes to script.js files

--- script.js
+++ script.js
@@ -13,14 +13,4 @@ function showForm(formId) {
     document.getElementById(formId).classList.add('active');
 }
 
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const role = document.getElementById('signup-role').value;
-    if (role === 'student') {
-        window.location.href = '';
-    } else if (role === 'parent') {
-        window.location.href = '';
-    } else if (role === 'educator') {
-        window.location.href = '';
-    }
-});
+
